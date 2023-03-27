f2b6cd7b4af98ed828566a49b410ed4793dcfb7a
nikitatuseev <tuseevn@mail.ru>
2023-03-27 17:32:05 +0300

    доработать валидацию

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 import javax.validation.constraints.*;
 import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 public class User {
