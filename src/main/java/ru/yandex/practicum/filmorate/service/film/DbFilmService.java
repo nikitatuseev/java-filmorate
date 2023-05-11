df6f84fefc6ce20ea16e9c3d42642d4d70ae3f17
nikitatuseev <tuseevn@mail.ru>
2023-05-11 15:33:45 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/service/film/DbFilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/DbFilmService.java
@@ -9,7 +9,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.user.UserService;
 
-import java.util.List;;
+import java.util.List;
 
 @Service("DbFilmService")
 public class DbFilmService implements FilmService {
